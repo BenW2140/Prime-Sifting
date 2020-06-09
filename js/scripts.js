8f0df42cd0e32e9d52aa64952865acd97b301683
Ian Scott <chesnekov@gmail.com>
2020-06-09 14:50:24 -0700

    js will take in and store user value, creates num array from 2-user input number
    Co-authored-by: Ben White <bwhite2140@outlook.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
-  $("form#stress-test").submit(function (event) {
+  $("form#prime-sifting").submit(function(event) {
+    event.preventDefault();
+
+    const limit = parseInt($("input#limit").val());
+    console.log(limit);
+    
+    let numList = [];
+    for (let i = 2; i <= limit; i++) {
+      numList.push(i);
+    }
+
+    console.log(numList);
+
+    // numList.forEach(function (number) {
+	
+    // });
 
   });
 });
